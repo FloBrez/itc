4a258d9b66eead17b28c91764c7677714aed0e61
Florian Brezina <florian.brezina@live.de>
2020-01-29 21:42:07 +0100

    added R and shell scripts

---
+++ build_gitbook.R
@@ -0,0 +1,3 @@
+require(bookdown)
+setwd("C:\\Users\\flori\\OneDrive\\Projekte\\intro_to_causality")
+bookdown::render_book("index.md", output_dir = "C:\\Users\\flori\\OneDrive\\Projekte\\intro_to_causality\\docs")
