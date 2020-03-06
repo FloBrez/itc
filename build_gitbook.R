fe12cbda6960c7afc1447685ac6658934f227cf8
Florian Brezina <florian.brezina@live.de>
2020-03-06 21:37:28 +0100

    added pdf rendering

--- build_gitbook.R
+++ build_gitbook.R
@@ -1,3 +1,4 @@
 require(bookdown)
 setwd("C:\\Users\\flori\\OneDrive\\Projekte\\intro_to_causality")
 bookdown::render_book("index.md", output_dir = "C:\\Users\\flori\\OneDrive\\Projekte\\intro_to_causality\\docs")
+bookdown::render_book("index.md", "bookdown::pdf_book", output_dir = "C:\\Users\\flori\\OneDrive\\Projekte\\intro_to_causality\\pdfs")
